ef9424ae3bcbe3d6a0549f81242849293428a7ce
KazukiYunoue <you.know.ue1480@gmail.com>
2019-08-16 15:48:27 +0900

    implemented API functions

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -10,8 +10,18 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'should be index as json' do
+    get entries_url, as: :json
+    assert_response :success
+  end
+
   test 'should show entry' do
     get entry_url(@entry)
+    assert_response :success
+  end
+
+  test 'should show entry as json' do
+    get entry_url(@entry), as: :json
     assert_response :success
   end
 
@@ -29,6 +39,15 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to entry_url(Entry.last)
   end
 
+  test 'should create entry as json' do
+    assert_difference('Entry.count') do
+      post entries_url, params: { entry: { body: @entry.body,
+                                           title: @entry.title } }, as: :json
+    end
+
+    assert_response :success
+  end
+
   test 'should get edit' do
     get edit_entry_url(@entry)
     assert_response :success
@@ -40,11 +59,25 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to entry_url(@entry)
   end
 
+  test 'should update entry as json' do
+    patch entry_url(@entry), params: {entry: { title: @entry.title,
+                                               body: @entry.body } }, as: :json
+    assert_response :success
+  end
+
   test 'should destroy entry' do
     assert_difference('Entry.count', -1) do
       delete entry_url(@entry)
     end
 
     assert_redirected_to entries_url
+  end
+
+  test 'should destroy entry as json' do
+    assert_difference('Entry.count', -1) do
+      delete entry_url(@entry), as: :json
+    end
+
+    assert_response :success
   end
 end
