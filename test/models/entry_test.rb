58eed895186fd0f1835fe64a1fddda0c1efe2a0d
Kazuki Yunoue <you.know.ue1480@gmail.com>
2019-08-17 15:14:47 +0900

    implemented validation for Entry

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,4 +1,19 @@
 class EntryTest < ActiveSupport::TestCase
-  # TODO: add presence validation
-  # TODO: add length validation
+  setup do
+    @entry = entries(:one)
+  end
+
+  test 'valid entry' do
+    assert @entry.valid?
+  end
+
+  test 'invalid without title' do
+    @entry.title = ''
+    assert_not @entry.valid?
+  end
+
+  test 'title should less than 140' do
+    @entry.title = '0123456789' * 15
+    assert_not @entry.valid?
+  end
 end
