889f966dd5713b02014849dc8c8bcdef7775bade
KazukiYunoue <you.know.ue1480@gmail.com>
2019-08-17 14:43:57 +0900

    injected TODOs

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,4 @@
 class Entry < ActiveRecord::Base
+  # TODO: add validation with presence
+  # TODO: add validation with length
 end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,2 +1,4 @@
 class EntryTest < ActiveSupport::TestCase
+  # TODO: add presence validation
+  # TODO: add length validation
 end
