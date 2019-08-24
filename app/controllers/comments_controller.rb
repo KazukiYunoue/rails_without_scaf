7eeb6fed75a3a25b9f55484ee98b714d1e7f5406
KazukiYunoue <you.know.ue1480@gmail.com>
2019-08-24 16:41:27 +0900

    modified TODOs for Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-  # TODO: add before_action set_entry
+  # TODO: add before_action set_comment
+  # TODO: add before_action set_entry_by_params
+  # TODO: add before_action set_entry_by_association
   # TODO: create
   # TODO: destroy
   # TODO: approve
@@ -8,4 +10,6 @@ class CommentsController < ApplicationController
 
   # TODO: set_comment
   # TODO: comment_params
+  # TODO: set_entry_by_params
+  # TODO: set_entry_by_association
 end
