5e2cf29405fb2d8d54ba63e43728d8a1fe256a34
KazukiYunoue <you.know.ue1480@gmail.com>
2019-08-25 15:26:28 +0900

    implemented comments_controller and controller test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,43 @@
 class CommentsController < ApplicationController
-  # TODO: add before_action set_comment
-  # TODO: add before_action set_entry_by_params
-  # TODO: add before_action set_entry_by_association
-  # TODO: create
-  # TODO: destroy
-  # TODO: approve
+  before_action :set_comment, only: %i[destroy approve]
+  before_action :set_entry_by_params, only: [:create]
+  before_action :set_entry_by_association, only: %i[destroy approve]
+
+  def create
+    @comment = @entry.comments.build(comment_params)
+
+    if @entry.save
+      redirect_to @entry, notice: 'Comment was successfully created.'
+    else
+      render :show, location: @entry
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to entry_url(@entry), notice: 'Comment was successfully destroyed.'
+  end
+
+  def approve
+    @comment.update(status: 'approved')
+    redirect_to entry_url(@entry), notice: 'Comment was successfully approved.'
+  end
 
   private
 
-  # TODO: set_comment
-  # TODO: comment_params
-  # TODO: set_entry_by_params
-  # TODO: set_entry_by_association
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :status)
+  end
+
+  def set_entry_by_params
+    @entry = Entry.find(params[:entry_id])
+  end
+
+  def set_entry_by_association
+    @entry = @comment.entry
+  end
 end
