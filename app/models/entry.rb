68056697082ab02d077f968297ebc355483995df
KazukiYunoue <you.know.ue1480@gmail.com>
2019-09-01 14:27:39 +0900

    added dependency to destroy associated records

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ActiveRecord::Base
   belongs_to :blog
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { maximum: 140 }
 end
